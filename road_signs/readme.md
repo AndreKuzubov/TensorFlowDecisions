a800a3e923c8fd9ea40b7e84016fb31658917322
Andrey Kuzubov <smoll9521@gmail.com>
2019-02-10 14:58:55 +0300

    road signs classifier

--- road_signs/readme.md
+++ road_signs/readme.md
@@ -2,5 +2,10 @@
 
 Распознавание дорожных знаков. 
 Датасет [GTSRB](http://benchmark.ini.rub.de/?section=gtsrb&subsection=news). 
-Нейронная сеть основана на базе сверточной сети [VGG](https://neurohive.io/ru/vidy-nejrosetej/vgg16-model/)
+Нейронная сеть основана на базе сверточной сети 
 
+[road_signs.py](road_signs.py)
+
+#### Источники
+ 
+ - [VGG](https://neurohive.io/ru/vidy-nejrosetej/vgg16-model/) 
--- road_signs/road_signs.py
+++ road_signs/road_signs.py
@@ -19,6 +19,25 @@ import os
 import zipfile
 import glob
 import random
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, Reshape
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+
+MODEL_PATH = "log/road_signs/road_signs_model.hdf5"
+if not os.path.exists(os.path.dirname(MODEL_PATH)):
+    os.makedirs(os.path.dirname(MODEL_PATH))
+
+INPUT_IMAGE_SIZE = 32
+
+
+def showImages(imagesX, batchY):
+    imagesX = imagesX.reshape((-1, INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE, 3))
+    imagesX *= 255.
+    for i, image2d in enumerate(imagesX):
+        image = Image.fromarray(image2d.astype(np.uint8))
+        image.show(title=str(i) + str(list(batchY[i]).index(1)))
 
 
 def loadDataSet():
@@ -49,7 +68,7 @@ def loadDataSet():
         print("success GTSRB_Final_Test_Images")
 
 
-def next_train_batch(batch_size=100):
+def next_batch(batch_size=100):
     files = glob.glob("dataset/GTSRB_Final_Training_Images/GTSRB/Final_Training/Images/*/*.ppm")
     random.shuffle(files)
 
@@ -59,7 +78,7 @@ def next_train_batch(batch_size=100):
         f = random.choice(files)
         image = Image.open(f)
         # TODO сделать сверку без приведения к единому размеру
-        image = image.resize((32, 32), Image.ANTIALIAS)
+        image = image.resize((INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE), Image.ANTIALIAS)
         x = np.asarray(image, dtype=float)
         x /= x.max()
         _sy = int(f[f.rfind("/") - 4:f.rfind("/")])
@@ -69,13 +88,53 @@ def next_train_batch(batch_size=100):
         x_batch += [x]
         y_batch += [y]
 
-    return x_batch, y_batch
+    return np.array(x_batch), np.array(y_batch)
 
+
+def train():
+    model = Sequential()
+    model.add(Convolution2D(32, kernel_size=(5, 5), padding='valid'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(3, 3), padding='valid'))
+
+    model.add(Convolution2D(64, kernel_size=(5, 5), padding='valid'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+
+    model.add(Flatten())
+    model.add(Dense(1024))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(43))
+    model.add(Activation('softmax'))
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+    for i in range(3):
+        batch_xs, batch_ys = next_batch(batch_size=2000)
+        test_xs, test_ys = next_batch(batch_size=100)
+
+        model.fit(batch_xs, batch_ys,
+                  verbose=1,
+                  epochs=10,
+                  validation_data=(test_xs, test_ys),
+                  validation_split=0.1,
+                  callbacks=[ModelCheckpoint(filepath=MODEL_PATH,
+                                             monitor="val_acc",
+                                             save_best_only=True,
+                                             save_weights_only=False,
+                                             mode="auto")
+                             ]
+                  )
+    print("finish...")
 
 
 if __name__ == "__main__":
     loadDataSet()
 
-    x_batch, y_batch = next_train_batch()
+    train()
 
-    print()
+    model = load_model(MODEL_PATH)
+    test_xs, test_ys = next_batch(batch_size=1000)
+    score = model.evaluate(test_xs, test_ys, verbose=0)
+    print("Test score %f " % score[0])
+    print("Test accuracy %f " % score[1])
